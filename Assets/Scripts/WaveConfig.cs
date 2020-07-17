2094b64897a5ded05eea03f749cd03668651d673
Carl Hain <crhain@gmail.com>
2020-07-17 13:53:19 -0500

    Refactor naming of private variables in WaveConfig.cs

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
 {
-    [SerializeField] GameObject enemyPrefab;
-    [SerializeField] GameObject pathPrefab;
-    [SerializeField] float timeBetweenSpawns = 0.5f;
-    [SerializeField] float spawnRandomFactor = 0.3f;
-    [SerializeField] int numberOfEnemies = 5;
-    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] GameObject _pathPrefab;
+    [SerializeField] float _timeBetweenSpawns = 0.5f;
+    [SerializeField] float _spawnRandomFactor = 0.3f;
+    [SerializeField] int _numberOfEnemies = 5;
+    [SerializeField] float _moveSpeed = 2f;
 
-    public GameObject GetEnemyPrefab(){ return enemyPrefab; }
+    public GameObject GetEnemyPrefab(){ return _enemyPrefab; }
 
     public List<Transform> GetWaypoints()
     {
         var waveWaypoints = new List<Transform>();
-        foreach(Transform child in pathPrefab.transform)
+        foreach(Transform child in _pathPrefab.transform)
         {
             waveWaypoints.Add(child);
         }
@@ -25,11 +25,11 @@ public class WaveConfig : ScriptableObject
         return waveWaypoints;
     }
 
-    public float GetTimeBetweenSpawns(){ return timeBetweenSpawns; }
+    public float GetTimeBetweenSpawns(){ return _timeBetweenSpawns; }
 
-    public float GetSpawnRandomFactor(){ return spawnRandomFactor; }
+    public float GetSpawnRandomFactor(){ return _spawnRandomFactor; }
 
-    public int GetNumberOfEnemies(){ return numberOfEnemies; }
+    public int GetNumberOfEnemies(){ return _numberOfEnemies; }
 
-    public float GetMoveSpeed(){ return moveSpeed; }
+    public float GetMoveSpeed(){ return _moveSpeed; }
 }
