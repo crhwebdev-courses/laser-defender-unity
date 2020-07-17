79f20ec2b557e192a92d82d7e012e8c2f7bf9c05
Carl Hain <crhain@gmail.com>
2020-07-17 12:09:03 -0500

    Add enemy health and damage from being hit by objects with the DamageDealer script

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    // private config params
+    [SerializeField] private int _damage = 100;
+
+    // properties
+    public int Damage => _damage;
+    
+
+    // TODO This is here for backwards compatibility
+    public int GetDamage()
+    {
+        return _damage;
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+}
