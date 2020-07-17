87f55457d119b156a436406523543eab2924f744
Carl Hain <crhain@gmail.com>
2020-07-17 13:24:56 -0500

    Enemies now fire lasers--but they do no damage to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float projectileSpeed = 10f;
+
+    [SerializeField] GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        Debug.Log("Shot counter: " + shotCounter.ToString());
+        if(shotCounter < 0f)
+        {
+            Debug.Log("Fired!");
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+                projectile,
+                transform.position,
+                Quaternion.identity) as GameObject;
+
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,7 +58,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         _health -= damageDealer.Damage;
-        damageDealer.Hit();
+        //damageDealer.Hit();
 
         if (_health <= 0)
         {
