afd3e23e7ac9653020f4df55341cffe0f47251f1
Carl Hain <crhain@gmail.com>
2020-07-17 18:27:27 -0500

    Add 'Game Over' and 'Start Menu' scenes; hook up all scenes to work together--currently does not switch to 'Game Over' when player dies

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private string _gameStartScene = "Game Start Scene";
+    [SerializeField] private string _gameScene = "Game";
+    [SerializeField] private string _gameOverScene = "Game Over Scene";
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(_gameStartScene);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(_gameScene);
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(_gameOverScene);
+    }
+            
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
